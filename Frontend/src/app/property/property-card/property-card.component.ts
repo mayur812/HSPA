f69b15f2c6091896802e1d17789f18b2d7668ae9
Mayur.Joshi <ayuroshi56@gmail.com>
2023-10-13 10:11:33 +0530

    making a property list

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-property-card",
@@ -8,10 +8,5 @@ import { Component } from "@angular/core";
 
 )
 export class PropertyCardComponent {
-    Property: any ={
-        "id" :1,
-        "name":"Skyline Property",
-        "type":"Residential",
-        "price":12000
-    }
+    @Input() Property : any
 }
